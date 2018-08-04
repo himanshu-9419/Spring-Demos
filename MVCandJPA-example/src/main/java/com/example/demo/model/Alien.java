640c7ab0778e75c70f1148f8bbcb9eb9154cd349
Himanshu Arora <himanshuarora9419@gmail.com>
2018-08-05 02:50:19 +0530

    adding crud operations using Crud repository

--- MVCandJPA-example/src/main/java/com/example/demo/model/Alien.java
+++ MVCandJPA-example/src/main/java/com/example/demo/model/Alien.java
@@ -8,6 +8,14 @@ public class Alien {
 	@Id
 	private int aid;
 	private String aname;
+	private String tech;
+	
+	public String getTech() {
+		return tech;
+	}
+	public void setTech(String tech) {
+		this.tech = tech;
+	}
 	public int getAid() {
 		return aid;
 	}
@@ -20,6 +28,12 @@ public class Alien {
 	public void setAname(String aname) {
 		this.aname = aname;
 	}
+	@Override
+	public String toString() {
+		return "Alien [aid=" + aid + ", aname=" + aname + ", tech=" + tech + "]";
+	}
+	
+	
 	
 	
 }
--- MVCandJPA-example/src/main/resources/data.sql
+++ MVCandJPA-example/src/main/resources/data.sql
@@ -1,1 +1,5 @@
-insert into alien values (101,'Himanshu');
+insert into alien values (101,'Himanshu1','cs1');
+insert into alien values (102,'Himanshu2','cs2');
+insert into alien values (103,'Himanshu3','cs3');
+insert into alien values (104,'Himanshu4','cs4');
+insert into alien values (105,'Himanshu5','cs5');
