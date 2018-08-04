926c6c6fb064d3b4128723aeab2b30b10dca2224
Himanshu Arora <himanshuarora9419@gmail.com>
2018-08-05 01:32:05 +0530

    Using ModelAndView

--- WebAppDemo/src/main/java/com/example/WebAppDemo/HomeController.java
+++ WebAppDemo/src/main/java/com/example/WebAppDemo/HomeController.java
@@ -1,20 +1,21 @@
 package com.example.WebAppDemo;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 	@RequestMapping("home")
-	public String home(String name,HttpSession session) {
+	public ModelAndView home(@RequestParam("name") String name) {
 		// we can remove req and res and add field I'm expecting and session
-		System.out.println("home "+name);
-		session.setAttribute("name", name);
-		return "home";
+		ModelAndView mv= new ModelAndView();
+		System.out.println("name is:"+name);
+		mv.addObject("name",name);
+		mv.setViewName("home");
+		return mv;
 	}
 }
--- WebAppDemo/src/main/webapp/pages/home.jsp
+++ WebAppDemo/src/main/webapp/pages/home.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
- I'm home.jsp Welcome ${sessionScope.name}
+ I'm home.jsp Welcome <% String name=(String)request.getAttribute("name"); out.println(name); %> 
 </body>
 </html>
