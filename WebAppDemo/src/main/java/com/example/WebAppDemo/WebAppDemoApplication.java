0e1ce9811939714d9f13e027dfc8c0cd85c44143
Himanshu Arora <himanshuarora9419@gmail.com>
2018-08-05 00:20:46 +0530

    web app demo with controller handling view run and visit local host 8080

---
+++ WebAppDemo/src/main/java/com/example/WebAppDemo/WebAppDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.WebAppDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppDemoApplication.class, args);
+	}
+}
