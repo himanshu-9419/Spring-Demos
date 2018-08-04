2975b20444657c5af1384b4e94cebc0eb2549ad5
Himanshu Arora <himanshuarora9419@gmail.com>
2018-08-04 23:44:53 +0530

    initial commit

---
+++ dependencyInjectionExample/src/main/java/com/example/demo/Alien.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package com.example.demo;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author HimanshuArora
+ *
+ */
+@Component
+@Scope(value="prototype")
+public class Alien {
+	private int add;
+	private String aName;
+	private String tech;
+	private Laptop laptop;
+	
+	
+	public Laptop getLaptop() {
+		return laptop;
+	}
+	public void setLaptop(Laptop laptop) {
+		this.laptop = laptop;
+	}
+	public Alien() {
+		super();
+		System.out.println("object created");
+	}
+	public int getAdd() {
+		return add;
+	}
+	public void setAdd(int add) {
+		this.add = add;
+	}
+	public String getaName() {
+		return aName;
+	}
+	public void setaName(String aName) {
+		this.aName = aName;
+	}
+	public String getTech() {
+		return tech;
+	}
+	public void setTech(String tech) {
+		this.tech = tech;
+	}
+	
+	public void show() {
+		System.out.println("show");
+		laptop.compile();
+	}
+
+}
---
+++ dependencyInjectionExample/src/main/java/com/example/demo/DependencyInjectionExampleApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class DependencyInjectionExampleApplication {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context= SpringApplication.run(DependencyInjectionExampleApplication.class, args);
+		Alien alien=context.getBean(Alien.class);
+		alien.show();
+	}
+}
