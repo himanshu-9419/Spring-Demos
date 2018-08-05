0f1d544f0f3b3e5a288f831a3ca45025465bd1eb
Himanshu Arora <himanshuarora9419@gmail.com>
2018-08-05 12:25:10 +0530

    maling rest api get

--- MVCandJPA-example/src/main/java/com/example/demo/controller/AlienController.java
+++ MVCandJPA-example/src/main/java/com/example/demo/controller/AlienController.java
@@ -2,8 +2,10 @@ package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.dao.AlienRepo;
@@ -34,5 +36,17 @@ public class AlienController {
 		System.out.println(repo.findAllByTechSorted("js"));
 		mv.addObject(alien);
 		return mv;
+	}
+	
+	@RequestMapping("/aliens")
+	@ResponseBody
+	public String getAlien() {
+		return repo.findAll().toString();
+	}
+	
+	@RequestMapping("/alien/{aid}")
+	@ResponseBody
+	public String getAlien(@PathVariable("aid") int aid) {
+		return repo.findById(aid).toString();
 	}
 }
